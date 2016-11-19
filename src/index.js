c608316753faf6fe8bedb23659a7594165abd150
Kron0S <kron00000s@gmail.com>
2016-11-19 09:33:04 +0500

    oher tests

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.get('/volumes', async (req, res) => {
   let hdds = {};
   if (!'hdd' in pc) {
-    return res.status(404).send('Not found');
+    return res.status(404).send('Not Found');
   }
 	for (let i = 0; i < pc.hdd.length; i++) {
     const hdd = pc.hdd[i];
@@ -29,14 +29,23 @@ app.get('/', async (req, res) => {
 	res.send(pc);
 });
 app.get(/\/.*/, async (req, res) => {
+  console.log(req.url);
 	const parts = req.url.replace(/^\/|\/$/g, '').split('/');
 	let answer = pc;
 	for (let i = 0; i < parts.length; i++) {
 		const part = parts[i];
-		if (part in answer) {
+    console.log(answer);
+    console.log(part);
+    console.log(answer.hasOwnProperty(part));
+    console.log(answer.constructor());
+    console.log(answer.constructor()[part]);
+    console.log(typeof answer[part]);
+    console.log(111);
+		if (answer.constructor()[part] === undefined) {
+      console.log('have');
 			answer = answer[part];
 		} else {
-			return res.status(404).send('Not found');
+			return res.status(404).send('Not Found');
 		}
 	}
 	return res.send(JSON.stringify(answer));
@@ -51,7 +60,7 @@ app.listen(3000, () => {
 		})
 		.catch(err => {
 			console.log('Чтото пошло не так:', err);
-      return res.status(404).send('Not found');
+      return res.status(404).send('Not Found');
 		});
 
 	console.log('Example app listening on port 3000!');
